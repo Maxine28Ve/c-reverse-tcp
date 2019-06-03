b6b2b74ef453f5f6338b822434a9e6d7ca4ae219
Alessandro Dussin <alessandro.dussin@gmail.com>
2019-06-03 10:28:45 +0200

    nice

--- triad_of_apocalypse.c
+++ triad_of_apocalypse.c
@@ -8,6 +8,47 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+short int port;
 int main(int argc, char** argv){
+
+	unsigned short int startport = 3306;
+	int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(startport);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    while(bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+    	if(startport <= 65535){
+		    server_addr.sin_port = htons(++startport);
+    	}
+    }
+    printf("Found a port: %hu\n", startport);
+    port = startport;
+
+	int pid = fork();
+	if(pid == 0){
+		pid = fork();
+		if(pid == 0){
+			pid = fork();
+		}
+	}
+	int i = 0;
+	sleep(2);
+	if(pid != 0){
+		printf("Process pid: %d\n", getpid());
+	    server_addr.sin_port = htons(port);
+		while(i < 10){
+			printf("Process %d tries to acquire the port %d\n", getpid(), port);
+		    while(bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+				printf("Process %d failed to connect\n", getpid());
+				sleep(rand()%3);
+		    }
+		    return 0;
+		}
+		printf("End %d\n", getpid());
+	}
+
+
 	return 0;
 }
